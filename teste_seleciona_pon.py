473f9fb5e9f4f4ac7b4ecf63fec6a79f3a1d2ab4
strvictor <vicctor1009@gmail.com>
2023-06-29 18:16:50 -0300

    up

--- teste_seleciona_pon.py
+++ teste_seleciona_pon.py
@@ -1,39 +1,36 @@
+
 jamic = 'R'
+bujaru = 'P'
 cto_lista  = []
+while 1:
+    cto = input('Informe a cto: ').upper().replace(' ','').strip()
 
-cto = input('Informe a cto: ').upper().replace(' ','').strip()
+    cto_lista.append(cto)
 
-cto_lista.append(cto)
+    if cto[0] == jamic or cto[0] == bujaru:
 
-if cto[0] == jamic:
+        print('cto é da jamic', cto_lista[0])
+        
+        slot = ord(cto[1]) - 64 # se deixar 65 a letra 'A' fica 0
 
-    print('cto é da jamic', cto_lista[0])
-    
-    slot = ord(cto[1]) - 64 # se deixar 65 a letra 'A' fica 0
+        pon = ord(cto[2]) - 64
 
-    pon = ord(cto[2]) - 64
+        print(f'slot: {slot}\npon: {pon}')
 
-    print(f'slot: {slot}\npon: {pon}')
+    else:
+        
+        slot = ord(cto[1]) - 65 # se deixar 65 a letra 'A' fica 0
 
+        pon = ord(cto[2]) - 65
 
-
-
-
-
-
-
-
-
-
+        print(f'slot: {slot}\npon: {pon}')
+    cto_lista.clear()
 
 
 
 
 
-def converte_p_numero(letra):
-    print(ord(letra.upper()) - 65)
-    return ord(letra.upper()) - 65
-    
+
 
 
 
